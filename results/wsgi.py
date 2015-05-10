ef3fcf8d4eb728dbee3f7775b375f183db5c66e8
KidEinstein <anirudhshekh@gmail.com>
2015-05-10 12:14:37 +0530

    Modified wsgi.py

--- results/wsgi.py
+++ results/wsgi.py
@@ -8,8 +8,12 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+sys.path.append('/var/www/MSRIT-Results')
+sys.path.append('/var/www/MSRIT-Results/results')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "results.settings")
 
