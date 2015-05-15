268705c4e5db21f875045dd5a0a3c6625e46b0ca
KidEinstein <anirudhshekh@gmail.com>
2015-05-16 00:34:32 +0530

    Change path

--- results/wsgi.py
+++ results/wsgi.py
@@ -12,8 +12,8 @@ import sys
 
 from django.core.wsgi import get_wsgi_application
 
-sys.path.append('/var/www/MSRIT-Results')
-sys.path.append('/var/www/MSRIT-Results/results')
+sys.path.append('/var/www/html/MSRIT-Results')
+sys.path.append('/var/www/html/MSRIT-Results/results')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "results.settings")
 
