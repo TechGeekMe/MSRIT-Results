53a26116c92766eac8e9b786fc4c64db13ede52a
KidEinstein <anirudhshekh@gmail.com>
2015-05-18 21:04:02 +0530

    Bug fix

--- results_app/urls.py
+++ results_app/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
 #    url(r'^update_db$', views.update_db, name='update_db'),
     url(r'^update_db/(?P<usn_base>.{7})/(?P<first_usn>\d{3})-(?P<last_usn>\d{3})/$', views.update_db, name='update_db'),
-    url(r'^update_db/(?P<year>.d{2})/$', views.update_db, name='update_db'),
+    url(r'^update_db/(?P<year>\d{2})/$', views.update_db, name='update_db'),
     url(r'^$', views.index, name='index'),
     url(r'^clean_db$', views.clean_db, name='clean_db'),
 ]
