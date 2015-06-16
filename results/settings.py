6ee0bfcd38b60ff6504ec2e5c2fef3866a8ccacc
KidEinstein <anirudhshekh@gmail.com>
2015-06-16 19:59:25 +0530

    Added caching

--- results/settings.py
+++ results/settings.py
@@ -133,3 +133,8 @@ STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
 # Tell the staticfiles app to use S3Boto storage when writing the collected static files (when
 # you run `collectstatic`).
 STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+
+AWS_HEADERS = {  # see http://developer.yahoo.com/performance/rules.html#expires
+    'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+    'Cache-Control': 'max-age=94608000',
+}
