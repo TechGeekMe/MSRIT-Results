52c9ac02321b68507473a5d6801053c0e01fb179
KidEinstein <anirudhshekh@gmail.com>
2015-06-18 18:24:49 +0530

    fab test

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,10 @@
-from fabric.api import local, settings, abort, run, cd
+from fabric.api import local, settings, abort, run, cd, env
 
-def prepare_deploy(message):
+env.hosts = ['ec2-52-25-23-160.us-west-2.compute.amazonaws.com']
+env.user = 'ec2-user'
+env.key_filename = '/Users/anirudh/Documents/Python-Projects/Results/key.pem'
+
+def prepare_deploy(message='fab push'):
     local('git add . && git commit'+" -m '"+message+"'")
     local('git push')
 
