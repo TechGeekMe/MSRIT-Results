668d3a4eab030defa38a72ef27a88c489020c304
KidEinstein <anirudhshekh@gmail.com>
2015-06-18 17:51:26 +0530

    Modified fabric script

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,11 @@
-def prepare_deploy:
+def prepare_deploy():
     local('git add . && git commit')
     local('git push')
-    
+
+def deploy():
+    code_dir = '/var/www/html/MSRIT-Results/'
+    with cd(code_dir):
+        run('git pull')
+        with cd(code_dir+'results'):
+            run('touch wsgi.py')
+        
