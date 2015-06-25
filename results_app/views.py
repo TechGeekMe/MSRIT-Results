36575510d976cb4a41357b48c4a9eeee47ed5cba
KidEinstein <anirudhshekh@gmail.com>
2015-06-26 00:09:33 +0530

    show hide first year working

--- results_app/views.py
+++ results_app/views.py
@@ -117,19 +117,22 @@ def subject_results(request):
     department = request.POST['department']
     course_code = request.POST['course_code']
     sort = request.POST['sort']
-    fybranch = request.POST['fybranch']
-    if department == 'FY':
-        subjects = Subject.objects.filter(course_code=course_code, result__student__branch_code=fybranch)
+    dic = {}
+    if 'fybranch' in request.POST:
+        fybranch = request.POST['fybranch']
+        dic.update({'fybranch': fybranch})
+        subjects = Subject.objects.filter(course_code=course_code, result__student__branch_code=fybranch)       
     else:
         subjects = Subject.objects.filter(course_code=course_code)
     if subjects.count() == 0:
-        return HttpResponseRedirect(reverse('results_app:student_not_found'))        
+        return HttpResponseRedirect(reverse('results_app:student_not_found'))
+    dic.update({'course_code': course_code, 'subject_name': subjects[0].subject_name, 'subjects': subjects, 'sort': sort, 'department': department})
     if sort == 'name':
         subjects = subjects.order_by('result__student__name')
     else:
         subjects = subjects.order_by('-grade_point')
-    
-    return render(request, 'results_app/subject_results.html', {'course_code': course_code, 'subject_name': subjects[0].subject_name, 'subjects': subjects, 'sort': sort, 'department': department, 'fybranch': fybranch})
+        
+    return render(request, 'results_app/subject_results.html', dic)
 
 def custom_404(request):
     return render(request, 'results_app/404.html')
