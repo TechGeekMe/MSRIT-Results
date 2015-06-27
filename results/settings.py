70c079adb8ac9236306529ff164d4792e8484d13
KidEinstein <anirudhshekh@gmail.com>
2015-06-27 19:57:14 +0530

    changed to mysql again

--- results/settings.py
+++ results/settings.py
@@ -77,25 +77,25 @@ WSGI_APPLICATION = 'results.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 
-##DATABASES = {
-##    'default': {
-##        'ENGINE': 'django.db.backends.mysql',
-##        'NAME': 'students',
-##        'USER': 'root',
-##        'PASSWORD': '***REMOVED***',
-##        'HOST': 'resultsdb.cninslv9ojgr.us-west-2.rds.amazonaws.com',
-##        'PORT': '3306'
-##        
-##    }
-##}
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'students',
+        'USER': 'root',
+        'PASSWORD': '***REMOVED***',
+        'HOST': 'resultsdb.cninslv9ojgr.us-west-2.rds.amazonaws.com',
+        'PORT': '3306'
+        
     }
 }
+
+
+##DATABASES = {
+##    'default': {
+##        'ENGINE': 'django.db.backends.sqlite3',
+##        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+##    }
+##}
 
 
 # Internationalization
