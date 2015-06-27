5b7d6f49532945e99b2560e3b0306023a4b2415d
KidEinstein <anirudhshekh@gmail.com>
2015-06-27 19:08:36 +0530

    removed to

--- results_app/result_fetcher.py
+++ results_app/result_fetcher.py
@@ -45,7 +45,7 @@ def fetch_result(usn):
     #Get html page
     
     payload = {'usn': usn, 'option': 'com_examresult', 'task': 'getResult'}
-    r = requests.post("http://exam.msrit.edu/index.php", data=payload, timeout=600)
+    r = requests.post("http://exam.msrit.edu/index.php", data=payload)
     data = r.text
     
     # Feed html to BeautifulSoup
