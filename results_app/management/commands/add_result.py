004015a1e179e24a6fcf13bbe445c49c5c982897
KidEinstein <anirudhshekh@gmail.com>
2016-01-23 01:24:32 +0530

    Improved command line

--- results_app/management/commands/add_result.py
+++ results_app/management/commands/add_result.py
@@ -4,20 +4,29 @@ from datetime import date
 from django.core.exceptions import ObjectDoesNotExist
 
 def add_usn(usn):
-    
-    r = result_fetcher.fetch_result(usn)
-    
-    # Check if the USN is non-existent
-    if r is None:
-        raise ValueError("USN %s" % usn)
+
     s = None
+    r = None
+    
     try:
         s = Student.objects.get(usn=usn)
+        if not Result.objects.filter(student=s, date=date(2016, 1, 1)).exists():
+            r = result_fetcher.fetch_result(usn)
+            # Check if the USN is non-existent
+            if r is None:
+                raise ValueError("USN %s" % usn)
+            put_result(s, r)
+            
     except ObjectDoesNotExist:
+        r = result_fetcher.fetch_result(usn)
+        # Check if the USN is non-existent
+        if r is None:
+            raise ValueError("USN %s" % usn)
         s = Student(usn=r.usn, name=r.name, department=r.department, branch_code=r.branch_code)
         s.save()
-        
-    if not Result.objects.filter(student=s, date=date(2016, 1, 1)).exists():
+        put_result(s, r)
+
+def put_result(s, r):
         result = Result(student=s, credits_registered=r.credits_registered, credits_earned=r.credits_earned, sgpa=r.sgpa, cgpa=r.cgpa, semester=r.semester if r.semester <= 8 else 8, date=date(2016, 1, 1))
         result.save()
         
@@ -30,3 +39,4 @@ def add_usn(usn):
                 subjectlist = SubjectList(course_code = sub.course_code,
                                           subject_name=sub.subject_name, department_code=sub.course_code[:2], first_year=True if sub.semester <= 2 else False)
                 subjectlist.save()
+    
