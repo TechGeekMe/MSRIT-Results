2bbdda94bce70712d2737419b1a2d03206ba15b1
KidEinstein <anirudhshekh@gmail.com>
2016-01-24 14:35:42 +0530

    Improved fetching code for MBA

--- results_app/management/commands/add_result.py
+++ results_app/management/commands/add_result.py
@@ -35,6 +35,6 @@ def put_result(s, r):
             subject.save()
             if not SubjectList.objects.filter(pk=sub.course_code).exists():
                 subjectlist = SubjectList(course_code = sub.course_code,
-                                          subject_name=sub.subject_name, department_code=sub.course_code[:2], first_year=True if sub.semester <= 2 and not re.match(r'^[A-Z][A-Z]+E', sub.course_code) else False)
+                                          subject_name=sub.subject_name, department_code=sub.course_code[:2], first_year=sub.first_year)
                 subjectlist.save()
     
