f2eabd64ea245ae481196a09e256e2e5e5bc4ca0
KidEinstein <anirudhshekh@gmail.com>
2016-01-25 12:37:52 +0530

    Create result_pinger.py

---
+++ results_app/management/commands/result_pinger.py
@@ -0,0 +1,8 @@
+from os import system
+from time import sleep
+import result_fetcher
+while (result_fetcher.fetch_results('1MS13CS001') == None):
+    print 'Not yet out'
+    sleep(5)
+print 'Results out'
+system("python /var/www/html/MSRIT-Results/manage.py fetch_results 13")
