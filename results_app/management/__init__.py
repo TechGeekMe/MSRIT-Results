76c50afe4e54f975c91d99275320c0589770166f
KidEinstein <anirudhshekh@gmail.com>
2016-01-25 21:32:10 +0530

    Added crude error handling

---
+++ results_app/management/__init__.py
@@ -0,0 +1,4 @@
+Python 2.7.9 (v2.7.9:648dcafa7e5f, Dec 10 2014, 10:10:46) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> 
