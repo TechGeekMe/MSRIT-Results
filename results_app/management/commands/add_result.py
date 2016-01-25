54d63a8cc9f591f3cc7aa6848d7fca90039ffc1a
KidEinstein <anirudhshekh@gmail.com>
2016-01-26 02:09:29 +0530

    Revert "Revert "Git pull""
    This reverts commit 00346486c31174fd264710432694645f2538b0ad.

--- results_app/management/commands/add_result.py
+++ results_app/management/commands/add_result.py
@@ -10,6 +10,10 @@ def add_usn(usn):
         s = Student.objects.get(usn=usn)
         if not Result.objects.filter(student=s, date=date(settings.TERM_YEAR, settings.TERM_MONTH, 1)).exists():
             r = result_fetcher.fetch_result(usn)
+            # Because department changes after first year
+            dept = r.department
+            s.department = dept
+            s.save()
             # Check if the USN is non-existent for that term
             if r is None:
                 raise ValueError("USN %s" % usn)
