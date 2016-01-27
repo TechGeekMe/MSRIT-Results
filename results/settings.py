8a6748fb5b72cd372a6a90199ca402fa54782eb3
KidEinstein <anirudhshekh@gmail.com>
2016-01-27 22:35:53 +0530

    Switched to AWS MySQL

--- results/settings.py
+++ results/settings.py
@@ -89,12 +89,12 @@ DATABASES = {
 }
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+##DATABASES = {
+##    'default': {
+##        'ENGINE': 'django.db.backends.sqlite3',
+##        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+##    }
+##}
 
 # Session
 
