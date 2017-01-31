6017008882fdf3cbe61d3d6976ef7526a8295ba4
KidEinstein <anirudhshekh@gmail.com>
2017-01-31 20:30:28 +0530

    Updated cookie

--- results_app/management/commands/result_fetcher.py
+++ results_app/management/commands/result_fetcher.py
@@ -46,7 +46,7 @@ def fetch_result(usn):
     try:
         #Get html page
         cookies = {'3e84ce0de6b7d1eb79699e2a6adfb3a1':	'9lit356u6en8h0j51hobj961d3'}
-        payload = {'usn': usn, 'option': 'com_examresult', 'task': 'getResult', 'osolCatchaTxt': 'THBTQ', 'osolCatchaTxtInst': '0'}
+        payload = {'usn': usn, 'option': 'com_examresult', 'task': 'getResult', 'osolCatchaTxt': 'EGQAP', 'osolCatchaTxtInst': '0'}
         r = requests.post("http://exam.msrit.edu/index.php", data=payload, cookies=cookies)
         data = r.text
         #print data
